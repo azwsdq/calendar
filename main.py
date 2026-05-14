9a3e280a8f4ceb4b356dcdb8103810b63a1f54ca
Sonik1107 <Sahok.nikol@mail.ru>
2026-05-15 00:56:19 +0300

    рофл

--- main.py
+++ main.py
@@ -268,3 +268,14 @@ async def edit_task(
         task.date = datetime.strptime(due_date, "%Y-%m-%d").date()
         db.commit()
     return RedirectResponse(url="/tasks", status_code=303)
+
+@app.get("/warning")
+async def warning_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "warning.html",
+        {}
+    )
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
